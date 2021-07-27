18e85ba649fed0680f8ddddf60ddbe64de42d470
Borislav <bdostov@gmail.com>
2021-07-27 14:53:29 +0300

    Work on ViewEngine - GenerateExecutableCode method

---
+++ BasicHttpServer.MvcFramework/ViewEngine/ErrorView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BasicHttpServer.MvcFramework.ViewEngine
+{
+    public class ErrorView : IView
+    {
+        private readonly IEnumerable<string> _errors;
+        private readonly string _csharpCode;
+
+        public ErrorView(IEnumerable<string> errors, string csharpCode)
+        {
+            _errors = errors;
+            _csharpCode = csharpCode;
+        }
+
+        public string ExecuteTemplate(object viewModel)
+        {
+            var html = new StringBuilder();
+            html.AppendLine($"<h1>View compile {_errors.Count()} errors:</h1><ul>");
+            foreach (var error in _errors)
+            {
+                html.AppendLine($"<li>{error}</li>");
+            }
+
+            html.AppendLine($"</ul><pre>{_csharpCode}</pre>");
+            return html.ToString();
+        }
+    }
+}
--- BasicHttpServer.MvcFramework/ViewEngine/ViewEngine.cs
+++ BasicHttpServer.MvcFramework/ViewEngine/ViewEngine.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
@@ -69,8 +72,23 @@ namespace ViewNamespace
 
             compileResult = compileResult.AddSyntaxTrees(SyntaxFactory.ParseSyntaxTree(csharpCode));
 
-            compileResult.Emit("view.dll");
-            return null;
+            using (var memoryStream = new MemoryStream())
+            {
+                var emitResult = compileResult.Emit(memoryStream);
+                if (!emitResult.Success)
+                {
+                    return new ErrorView(emitResult.Diagnostics
+                            .Where(e => e.Severity == DiagnosticSeverity.Error)
+                            .Select(e => e.GetMessage()), csharpCode);
+                }
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                var byteAssembly = memoryStream.ToArray();
+                var assembly = Assembly.Load(byteAssembly);
+                var viewType = assembly.GetType("ViewNamespace.ViewClass");
+                var instance = Activator.CreateInstance(viewType);
+                return instance as IView;
+            }
 
             // Roslyn
             // C# -> executable -> IView -> ExecuteTemplate
