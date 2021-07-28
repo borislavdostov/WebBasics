2ba0b90cf265d2f99323c3513dce6daf2703cb58
Borislav <bdostov@gmail.com>
2021-07-28 10:02:16 +0300

    Register static files in routeTable automatically

--- Apps/MyFirstMvcApp/Controllers/HomeController.cs
+++ Apps/MyFirstMvcApp/Controllers/HomeController.cs
@@ -1,11 +1,24 @@
-﻿using BasicHttpServer.HTTP;
+﻿using System;
+using BasicHttpServer.HTTP;
 using BasicHttpServer.MvcFramework;
+using MyFirstMvcApp.ViewModels;
 
 namespace MyFirstMvcApp.Controllers
 {
     public class HomeController : Controller
     {
         public HttpResponse Index(HttpRequest request)
+        {
+            var viewModel = new IndexViewModel
+            {
+                CurrentYear = DateTime.UtcNow.Year,
+                Message = "Welcome to Battle Cards"
+            };
+
+            return View(viewModel);
+        }
+
+        public HttpResponse About(HttpRequest request)
         {
             return View();
         }
--- BasicHttpServer.MvcFramework/Host.cs
+++ BasicHttpServer.MvcFramework/Host.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using BasicHttpServer.HTTP;
 
@@ -9,8 +11,41 @@ namespace BasicHttpServer.MvcFramework
         public static async Task CreateHostAsync(IMvcApplication application, int port = 80)
         {
             var routeTable = new List<Route>();
+
+            var staticFiles = Directory.GetFiles("wwwroot", "*", SearchOption.AllDirectories);
+            foreach (var staticFile in staticFiles)
+            {
+                var url = staticFile.Replace("wwwroot", string.Empty).Replace("\\", "/");
+                routeTable.Add(new Route(url, HttpMethod.Get, (request) =>
+                {
+                    var fileContent = File.ReadAllBytes(staticFile);
+                    var fileExtension = new FileInfo(staticFile).Extension;
+                    var contentType = fileExtension switch
+                    {
+                        ".txt" => "text/plain",
+                        ".js" => "text/javascript",
+                        ".css" => "text/css",
+                        ".jpg" => "image/jpg",
+                        ".jpeg" => "image/jpg",
+                        ".png" => "image/png",
+                        ".gif" => "image/gif",
+                        ".ico" => "image/vnd.microsoft.icon",
+                        ".html" => "text/html",
+                        _ => "text/plain"
+                    };
+
+                    return new HttpResponse(contentType, fileContent, HttpStatusCode.Ok);
+                }));
+            }
+
             application.ConfigureServices();
             application.Configure(routeTable);
+
+            Console.WriteLine("All registered routes:");
+            foreach (var route in routeTable)
+            {
+                Console.WriteLine($"{route.Method} {route.Path}");
+            }
 
             IHttpServer server = new HttpServer(routeTable);
 
